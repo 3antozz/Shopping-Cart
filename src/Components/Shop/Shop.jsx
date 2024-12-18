5b63088bdb36f56f0c5e7e5f3332f1d7488d66c3
3antozz <106344848+3antozz@users.noreply.github.com>
2024-12-18 18:50:52 +0100

    add search feature

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -3,12 +3,24 @@ import ProductCard from "../Product-Card/Product-Card";
 import PropTypes from "prop-types";
 import { useOutletContext } from "react-router-dom";
 import { useState } from "react";
-import { LoaderCircle } from 'lucide-react';
+import { LoaderCircle, Search } from "lucide-react";
 export default function Shop() {
     const { products, error, loading, handleAddToCart } = useOutletContext();
     const [popup, setPopup] = useState([]);
+    const [inputValue, setInputValue] = useState("");
+    const [filtered, setFiltered] = useState([]);
 
-    function handlePopup () {
+    function handleSearch (event) {
+        setInputValue(event.target.value);
+        if (event.target.value === "") {
+            setFiltered(products);
+        } else {
+            const filteredProducts = products.filter((product) => product.title.toLowerCase().includes(event.target.value.toLowerCase()));
+            setFiltered(filteredProducts);
+        }
+    }
+
+    function handlePopup() {
         setPopup((prev) => [...prev, true]);
         setTimeout(() => {
             setPopup((prev) => {
@@ -16,26 +28,58 @@ export default function Shop() {
                 curr.pop();
                 return curr;
             });
-        }, 4000)
+        }, 4000);
     }
 
     if (error)
-        return <div className={styles.error}><h1>Oops, an Error has Occured! Please try again later</h1></div>;
-    if (loading) return <div className={styles.loading}><LoaderCircle className={styles.spinner} size={75} color="rgba(255, 0, 0, 0.745)"/></div>;
+        return (
+            <div className={styles.error}>
+                <h1>Oops, an Error has Occured! Please try again later</h1>
+            </div>
+        );
+    if (loading)
+        return (
+            <div className={styles.loading}>
+                <LoaderCircle
+                    className={styles.spinner}
+                    size={75}
+                    color="rgba(255, 0, 0, 0.745)"
+                />
+            </div>
+        );
     return (
         <div className={styles.shop}>
-            {popup.length > 0 &&
-            <div className={styles.popups}>
-                {popup.map((pop, index) => <div key={index} className={styles.popup}>Item added to the cart!</div>)} 
-            </div>}
-            {products.map((product) => (
-                <ProductCard
-                    key={product.id}
-                    product={product}
-                    onSubmit={handleAddToCart}
-                    handlePopup={handlePopup}
+            <form className={styles.search}>
+                <label htmlFor="search"></label>
+                <input
+                    type="text"
+                    id="search"
+                    placeholder="Search for a product"
+                    value={inputValue}
+                    onChange={handleSearch}
                 />
-            ))}
+                <Search size={24} />
+            </form>
+            <div className={styles.products}>
+                {popup.length > 0 && (
+                    <div className={styles.popups}>
+                        {popup.map((pop, index) => (
+                            <div key={index} className={styles.popup}>
+                                Item added to the cart!
+                            </div>
+                        ))}
+                    </div>
+                )}
+
+                {filtered.map((product) => (
+                    <ProductCard
+                        key={product.id}
+                        product={product}
+                        onSubmit={handleAddToCart}
+                        handlePopup={handlePopup}
+                    />
+                ))}
+            </div>
         </div>
     );
 }
